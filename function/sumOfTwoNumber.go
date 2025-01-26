1d5e0c1fc8b6bfcc64483dcf270312cd4ff3008e
playgit2001 <108321233+playgit2001@users.noreply.github.com>
2025-01-26 22:40:44 +0800

    两数之和

--- function/sumOfTwoNumber.go
+++ function/sumOfTwoNumber.go
@@ -26,3 +26,13 @@ func TwoNum2(nums []int, target int) []int {
 	}
 	return nil
 }
+
+//测试用例
+//func main() {
+//	var nums []int = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+//	var result []int
+//	result = function.TwoNum2(nums, 11)
+//	for _, v := range result {
+//		println(v + 1)
+//	}
+//}
--- main.go
+++ main.go
@@ -5,10 +5,6 @@ import (
 )
 
 func main() {
-	var nums []int = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	var result []int
-	result = function.TwoNum2(nums, 11)
-	for _, v := range result {
-		println(v + 1)
-	}
+	var s int = 12121
+	print(function.IsPalindrome1(s))
 }
