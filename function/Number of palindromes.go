29ae08e84ce73ee491af136f65103b95cd6c627c
playgit2001 <108321233+playgit2001@users.noreply.github.com>
2025-01-26 22:41:36 +0800

    回文数1

--- function/Number of palindromes.go
+++ function/Number of palindromes.go
@@ -7,7 +7,7 @@ import (
 
 // 给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
 // 回文数 是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
-// 方法1 使用枚举的方式，从前后遍历，判断是否符合
+// 方法1 使用枚举的方式，从前后遍历，判断是否符合.
 func IsPalindrome1(x int) bool {
 	var strs string = strconv.Itoa(x)
 	for i := 0; i < len(strs)/2; i++ {
